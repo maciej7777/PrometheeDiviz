9fb8da4f5f293289d2c5d211a681dba32c48b753
Maciej Uniejewski <maciej.uniejewski@gmail.com>
2016-12-15 20:44:03 +0100

    [FlowSortGDSS]:
    - added sorting with central profiles

--- FlowSortGDSS/src/pl/poznan/put/promethee/FlowSortGDSS.java
+++ FlowSortGDSS/src/pl/poznan/put/promethee/FlowSortGDSS.java
@@ -53,6 +53,8 @@ public class FlowSortGDSS {
         for (int i = 0; i < inputs.alternativesIds.size(); i++) {
             Integer firstClassNumber = null;
             Integer secondClassNumber = null;
+            Set<Integer> firstClassDecisionMakers = new HashSet<>();
+            Set<Integer> secondClassDecisionMakers = new HashSet<>();
             String alternativeId = inputs.alternativesIds.get(i);
             for (int decisionMaker = 0; decisionMaker < inputs.profilesIds.size(); decisionMaker++) {
                 Integer decisionMakerClassNumber = null;
@@ -72,8 +74,12 @@ public class FlowSortGDSS {
                 }
                 if (firstClassNumber == null) {
                     firstClassNumber = decisionMakerClassNumber;
+                    firstClassDecisionMakers.add(decisionMaker);
                 } else if (firstClassNumber.intValue() != decisionMakerClassNumber) {
                     secondClassNumber = decisionMakerClassNumber;
+                    secondClassDecisionMakers.add(decisionMaker);
+                } else {
+                    firstClassDecisionMakers.add(decisionMaker);
                 }
             }
 
@@ -92,15 +98,21 @@ public class FlowSortGDSS {
                 interval.put("UPPER", rightClassId);
                 firstStepAssignments.put(alternativeId, interval);
 
-                List<Double> profilesFlowsWeightedSum = countWeightedSumForLimitingProfiles(alternativeId, inputs);
+                Double profileK;
+                Double profileK1;
 
-                Double profileK = profilesFlowsWeightedSum.get(Math.min(firstClassNumber, secondClassNumber));
-                Double profileK1 = profilesFlowsWeightedSum.get(Math.max(firstClassNumber, secondClassNumber));
+                if (firstClassNumber.intValue() < secondClassNumber.intValue()) {
+                    profileK = countDkForLimitingProfiles(alternativeId, firstClassNumber, firstClassDecisionMakers, inputs);
+                    profileK1 = countDk1ForLimitingProfiles(alternativeId, secondClassNumber, secondClassDecisionMakers, inputs);
+                } else {
+                    profileK = countDkForLimitingProfiles(alternativeId, secondClassNumber, secondClassDecisionMakers, inputs);
+                    profileK1 = countDk1ForLimitingProfiles(alternativeId, firstClassNumber, firstClassDecisionMakers, inputs);
+                }
 
-                if (profileK - profileK1 > 0) {
-                    assignments.put(alternativeId, leftClassId);
-                } else if (profileK - profileK1 < 0) {
+                if (profileK1 - profileK > 0) {
                     assignments.put(alternativeId, rightClassId);
+                } else if (profileK1 - profileK < 0) {
+                    assignments.put(alternativeId, leftClassId);
                 } else {
                     if (inputs.assignToABetterClass) {
                         assignments.put(alternativeId, rightClassId);
@@ -114,23 +126,112 @@ public class FlowSortGDSS {
         output.firstStepAssignments = firstStepAssignments;
     }
 
-    private static List<Double> countWeightedSumForLimitingProfiles(String alternativeId, InputsHandler.Inputs inputs) {
-        List<Double> profilesFlowsWeightedSum = new ArrayList<>();
+    private static double countDkForLimitingProfiles(String alternativeId, Integer profile, Set<Integer> decisionMakers, InputsHandler.Inputs inputs) {
+        double sum = 0.0;
 
-        //For each profile
-        for (int i = 0; i < inputs.profilesIds.get(0).size(); i++) {
-            Double sum = 0.0;
-            //For each decision maker
-            for (int j = 0; j < inputs.profilesIds.size(); j++) {
-                String profileId = inputs.profilesIds.get(j).get(i);
-                sum += inputs.decisionMakersWages.get(j) * inputs.profilesSummaryFlows.get(profileId).get(alternativeId);
-            }
-            profilesFlowsWeightedSum.add(sum / inputs.profilesIds.size());
+        for (Integer decisionMaker: decisionMakers) {
+            String profileId = inputs.profilesIds.get(decisionMaker).get(profile);
+            sum += inputs.decisionMakersWages.get(decisionMaker) * (inputs.alternativesFlowsAverage.get(alternativeId) - inputs.profilesSummaryFlows.get(profileId).get(alternativeId));
         }
-        return profilesFlowsWeightedSum;
+
+        return sum;
+    }
+
+    private static double countDk1ForLimitingProfiles(String alternativeId, Integer profile, Set<Integer> decisionMakers, InputsHandler.Inputs inputs) {
+        double sum = 0.0;
+
+        for (Integer decisionMaker: decisionMakers) {
+            String profileId = inputs.profilesIds.get(decisionMaker).get(profile);
+            sum += inputs.decisionMakersWages.get(decisionMaker) * (inputs.profilesSummaryFlows.get(profileId).get(alternativeId) - inputs.alternativesFlowsAverage.get(alternativeId));
+        }
+
+        return sum;
     }
 
     private static void sortWithCentralProfiles(InputsHandler.Inputs inputs, OutputsHandler.Output output) {
+        Map<String, String> assignments = new LinkedHashMap<>();
+        Map<String, Map<String, String>> firstStepAssignments = new LinkedHashMap<>();
 
+        for (int i = 0; i < inputs.alternativesIds.size(); i++) {
+            Integer firstClassNumber = null;
+            Integer secondClassNumber = null;
+            Set<Integer> firstClassDecisionMakers = new HashSet<>();
+            Set<Integer> secondClassDecisionMakers = new HashSet<>();
+            String alternativeId = inputs.alternativesIds.get(i);
+            for (int decisionMaker = 0; decisionMaker < inputs.profilesIds.size(); decisionMaker++) {
+                Integer nearestCalss = null;
+                double distance = 0.0;
+                for (int profile = 0; profile < inputs.profilesIds.get(decisionMaker).size(); profile++) {
+                    String profilesId = inputs.profilesIds.get(decisionMaker).get(profile);
+                    double tmpDist = Math.abs(inputs.profilesSummaryFlows.get(profilesId).get(alternativeId) - inputs.alternativesFlowsAverage.get(alternativeId));
+
+                    if (tmpDist < distance || nearestCalss == null) {
+                        nearestCalss = profile;
+                        distance = tmpDist;
+                    }
+                }
+
+                if (firstClassNumber == null) {
+                    firstClassNumber = nearestCalss;
+                    firstClassDecisionMakers.add(decisionMaker);
+                } else if (firstClassNumber.intValue() != nearestCalss.intValue()) {
+                    secondClassNumber = nearestCalss;
+                    secondClassDecisionMakers.add(decisionMaker);
+                } else {
+                    firstClassDecisionMakers.add(decisionMaker);
+                }
+            }
+            if (secondClassNumber == null) {
+                String classId = inputs.categoryProfiles.get(0).get(firstClassNumber).getCategory().id();
+                assignments.put(alternativeId, classId);
+                Map<String, String> interval = new LinkedHashMap<>();
+                interval.put("LOWER",classId);
+                interval.put("UPPER", classId);
+                firstStepAssignments.put(alternativeId, interval);
+            } else {
+                String leftClassId = inputs.categoryProfiles.get(0).get(Math.min(firstClassNumber, secondClassNumber)).getCategory().id();
+                String rightClassId = inputs.categoryProfiles.get(0).get(Math.max(firstClassNumber, secondClassNumber)).getCategory().id();
+                Map<String, String> interval = new LinkedHashMap<>();
+                interval.put("LOWER",leftClassId);
+                interval.put("UPPER", rightClassId);
+                firstStepAssignments.put(alternativeId, interval);
+
+                Double profileK;
+                Double profileK1;
+
+                if (firstClassNumber.intValue() < secondClassNumber.intValue()) {
+                    profileK = countDkForCentralProfiles(alternativeId, firstClassNumber, firstClassDecisionMakers, inputs);
+                    profileK1 = countDkForCentralProfiles(alternativeId, secondClassNumber, secondClassDecisionMakers, inputs);
+                } else {
+                    profileK = countDkForCentralProfiles(alternativeId, secondClassNumber, secondClassDecisionMakers, inputs);
+                    profileK1 = countDkForCentralProfiles(alternativeId, firstClassNumber, firstClassDecisionMakers, inputs);
+                }
+
+                if (profileK1 - profileK > 0) {
+                    assignments.put(alternativeId, rightClassId);
+                } else if (profileK - profileK1 < 0) {
+                    assignments.put(alternativeId, leftClassId);
+                } else {
+                    if (inputs.assignToABetterClass) {
+                        assignments.put(alternativeId, rightClassId);
+                    } else {
+                        assignments.put(alternativeId, leftClassId);
+                    }
+                }
+            }
+        }
+        output.assignments = assignments;
+        output.firstStepAssignments = firstStepAssignments;
+    }
+
+    private static double countDkForCentralProfiles(String alternativeId, Integer profile, Set<Integer> decisionMakers, InputsHandler.Inputs inputs) {
+        double sum = 0.0;
+
+        for (Integer decisionMaker: decisionMakers) {
+            String profileId = inputs.profilesIds.get(decisionMaker).get(profile);
+            sum += inputs.decisionMakersWages.get(decisionMaker) * Math.abs(inputs.profilesSummaryFlows.get(profileId).get(alternativeId) - inputs.alternativesFlowsAverage.get(alternativeId));
+        }
+
+        return sum;
     }
 }
