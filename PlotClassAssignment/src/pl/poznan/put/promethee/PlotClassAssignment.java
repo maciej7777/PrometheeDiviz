e7ed881ce504a8c42da25634d5f70bd37ae78b4e
Maciej Uniejewski <maciej.uniejewski@gmail.com>
2016-12-26 20:49:25 +0100

    [PlotCalssAssignment]:
    -created mandatory classes
    -added inputs data reading

---
+++ PlotClassAssignment/src/pl/poznan/put/promethee/PlotClassAssignment.java
@@ -0,0 +1,7 @@
+package pl.poznan.put.promethee;
+
+/**
+ * Created by Maciej Uniejewski on 2016-12-26.
+ */
+public class PlotClassAssignment {
+}
---
+++ PlotClassAssignment/src/pl/poznan/put/promethee/xmcda/OutputsHandler.java
@@ -0,0 +1,7 @@
+package pl.poznan.put.promethee.xmcda;
+
+/**
+ * Created by Maciej Uniejewski on 2016-12-26.
+ */
+public class OutputsHandler {
+}
