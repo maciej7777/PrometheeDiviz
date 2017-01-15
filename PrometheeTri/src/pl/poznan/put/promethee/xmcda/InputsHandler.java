8f09303b561087528ff087f7e328f12aa5ef4817
Maciej Uniejewski <maciej.uniejewski@gmail.com>
2017-01-15 20:47:13 +0100

    [PrometheeTri]:
    - refactored module to read preferences, criteria, and criteria weights instead of alternatives flows
    - added new description-wsDD files
    - added first tests

--- PrometheeTri/src/pl/poznan/put/promethee/xmcda/InputsHandler.java
+++ PrometheeTri/src/pl/poznan/put/promethee/xmcda/InputsHandler.java
@@ -1,6 +1,14 @@
 package pl.poznan.put.promethee.xmcda;
 
 import org.xmcda.*;
+import org.xmcda.Alternative;
+import org.xmcda.CategoriesProfiles;
+import org.xmcda.CategoriesValues;
+import org.xmcda.Category;
+import org.xmcda.CategoryProfile;
+import org.xmcda.Criterion;
+import org.xmcda.XMCDA;
+import org.xmcda.utils.Coord;
 
 import java.math.BigDecimal;
 import java.util.*;
@@ -19,10 +27,14 @@ public class InputsHandler {
         private List<String> alternativesIds;
         private List<String> categoriesIds;
         private List<String> profilesIds;
-        private Map<String, BigDecimal> flows;
+        private List<String> criteriaIds;
         private Boolean assignToABetterClass;
+        private Boolean useMarginalValue;
+        private Map<String, BigDecimal> criteriaWeights;
         private Map<String, Integer> categoriesRanking;
         private List<CategoryProfile> categoryProfiles;
+        private Map<String, Map<String, Map<String, BigDecimal>>> partialPreferences;
+
 
         public List<String> getAlternativesIds() {
             return alternativesIds;
@@ -48,14 +60,6 @@ public class InputsHandler {
             this.profilesIds = profilesIds;
         }
 
-        public Map<String, BigDecimal> getFlows() {
-            return flows;
-        }
-
-        public void setFlows(Map<String, BigDecimal> flows) {
-            this.flows = flows;
-        }
-
         public Boolean getAssignToABetterClass() {
             return assignToABetterClass;
         }
@@ -79,6 +83,38 @@ public class InputsHandler {
         public void setCategoryProfiles(List<CategoryProfile> categoryProfiles) {
             this.categoryProfiles = categoryProfiles;
         }
+
+        public Map<String, Map<String, Map<String, BigDecimal>>> getPartialPreferences() {
+            return partialPreferences;
+        }
+
+        public void setPartialPreferences(Map<String, Map<String, Map<String, BigDecimal>>> partialPreferences) {
+            this.partialPreferences = partialPreferences;
+        }
+
+        public List<String> getCriteriaIds() {
+            return criteriaIds;
+        }
+
+        public void setCriteriaIds(List<String> criteriaIds) {
+            this.criteriaIds = criteriaIds;
+        }
+
+        public Boolean getUseMarginalValue() {
+            return useMarginalValue;
+        }
+
+        public void setUseMarginalValue(Boolean useMarginalValue) {
+            this.useMarginalValue = useMarginalValue;
+        }
+
+        public Map<String, BigDecimal> getCriteriaWeights() {
+            return criteriaWeights;
+        }
+
+        public void setCriteriaWeights(Map<String, BigDecimal> criteriaWeights) {
+            this.criteriaWeights = criteriaWeights;
+        }
     }
 
     public static Inputs checkAndExtractInputs(XMCDA xmcda, ProgramExecutionResult xmcdaExecResults) {
@@ -96,7 +132,8 @@ public class InputsHandler {
         checkAndExtractCategories(inputs, xmcda, errors);
         checkCategoriesRanking(inputs, xmcda, errors);
         checkAndExtractProfilesIds(inputs, xmcda, errors);
-        checkAndExtractAlternativesFlows(inputs, xmcda, errors);
+        checkAndExtractCriteria(inputs, xmcda, errors);
+        checkAndExtractPartialPreferences(inputs, xmcda, errors);
 
         return inputs;
     }
@@ -124,12 +161,13 @@ public class InputsHandler {
             errors.addError("No programParameter found.");
             return;
         }
-        if (xmcda.programParametersList.get(0).size() != 1) {
-            errors.addError("Parameter's list must contain exactly one element.");
+        if (xmcda.programParametersList.get(0).size() != 2) {
+            errors.addError("Parameter's list must contain exactly two elements.");
             return;
         }
 
         checkAndExtractAssignToABetterClass(inputs, xmcda, errors);
+        checkAndExtractUseMarginalValue(inputs, xmcda, errors);
     }
 
     protected static void checkAndExtractAssignToABetterClass(Inputs inputs, XMCDA xmcda, ProgramExecutionResult errors) {
@@ -153,6 +191,31 @@ public class InputsHandler {
             inputs.setAssignToABetterClass(assignToABetterClass);
         } catch (Exception exception) {
             String err = "Invalid value for parameter assignToABetterClass, it must be true or false.";
+            errors.addError(err);
+        }
+    }
+
+    protected static void checkAndExtractUseMarginalValue(Inputs inputs, XMCDA xmcda, ProgramExecutionResult errors) {
+        Boolean useMarginalValue;
+
+        final ProgramParameter<?> prgParam = xmcda.programParametersList.get(0).get(1);
+        if (!"useMarginalValue".equalsIgnoreCase(prgParam.id())) {
+            errors.addError(String.format("Invalid parameter w/ id '%s'", prgParam.id()));
+            return;
+        }
+        if (prgParam.getValues() == null || (prgParam.getValues() != null && prgParam.getValues().size() != 1)) {
+            errors.addError("Parameter useMarginalValue must have a single (boolean) value only");
+            return;
+        }
+        try {
+            useMarginalValue = (Boolean) prgParam.getValues().get(0).getValue();
+            if (useMarginalValue == null) {
+                errors.addError("Invalid value for parameter useMarginalValue, it must be true or false.");
+                return;
+            }
+            inputs.setUseMarginalValue(useMarginalValue);
+        } catch (Exception exception) {
+            String err = "Invalid value for parameter useMarginalValue, it must be true or false.";
             errors.addError(err);
         }
     }
@@ -272,7 +335,128 @@ public class InputsHandler {
         }
     }
 
-    protected static void checkAndExtractAlternativesFlows(Inputs inputs, XMCDA xmcda, ProgramExecutionResult errors) {
+    protected static void checkAndExtractCriteria(Inputs inputs, XMCDA xmcda, ProgramExecutionResult errors) {
+        if (xmcda.criteria.getActiveCriteria().isEmpty()) {
+            errors.addError("You need to provide a not empty criteria list.");
+            return;
+        }
+        inputs.setCriteriaIds(xmcda.criteria.getActiveCriteria().stream().filter(a -> "criteria".equals(a.getMarker())).map(
+                Criterion::id).collect(Collectors.toList()));
+
+        checkAndExtractCriteriaWeights(inputs, xmcda, errors);
+    }
+
+    protected static void checkAndExtractCriteriaWeights(Inputs inputs, XMCDA xmcda, ProgramExecutionResult errors) {
+        if (xmcda.criteriaValuesList.size() != 1) {
+            errors.addError("You need to provide 1 alternatives values list for criteria weights.");
+            return;
+        }
+        checkAndExtractCriteriaFromList(inputs, xmcda, errors);
+    }
+
+    protected static void checkAndExtractCriteriaFromList(Inputs inputs, XMCDA xmcda, ProgramExecutionResult errors) {
+
+        inputs.setCriteriaWeights(new LinkedHashMap<>());
+
+        org.xmcda.CriteriaValues criteriaWeights = xmcda.criteriaValuesList.get(0);
+        if (!criteriaWeights.isNumeric()) {
+            errors.addError("Each criterion weight must have numeric type.");
+            return;
+        }
+
+        try {
+            Map<Criterion, LabelledQValues<Double>> criteriaWeightsMap = criteriaWeights;
+            for (Map.Entry<Criterion, LabelledQValues<Double>> weight : criteriaWeightsMap.entrySet()) {
+                Double tmpValue = weight.getValue().get(0).convertToDouble().getValue();
+                BigDecimal bigDecimalValue = BigDecimal.valueOf(tmpValue);
+                inputs.getCriteriaWeights().put(weight.getKey().id(), bigDecimalValue);
+            }
+        } catch (Exception exception) {
+            errors.addError("An error occurred: " + exception + ". Each flow must have numeric type.");
+            return;
+        }
+
+        checkMissingValuesInCriteriaWeights(inputs, errors);
+    }
+
+    protected static void checkMissingValuesInCriteriaWeights(Inputs inputs, ProgramExecutionResult errors) {
+        for (int j = 0; j < inputs.getCriteriaIds().size(); j++) {
+            String criterionId = inputs.getCriteriaIds().get(j);
+            if (!inputs.getCriteriaWeights().containsKey(criterionId)) {
+                errors.addError("There are some missing values in criteria weights.");
+                return;
+            }
+        }
+    }
+
+    protected static void checkAndExtractPartialPreferences(Inputs inputs, XMCDA xmcda, ProgramExecutionResult errors) {
+        if (xmcda.alternativesMatricesList.size() != 1) {
+            errors.addError("You need to provide 1 alternatives values list for partial preferences.");
+            return;
+        }
+        checkAndExtractPreferences(inputs, xmcda, errors);
+    }
+
+    protected static void checkAndExtractPreferences(Inputs inputs, XMCDA xmcda, ProgramExecutionResult errors) {
+
+        inputs.partialPreferences = new LinkedHashMap<>();
+        @SuppressWarnings("unchecked")
+        AlternativesMatrix<Double> matrix = (AlternativesMatrix<Double>) xmcda.alternativesMatricesList.get(0);
+
+        List<String> alternativesAndProfiles = new ArrayList<>();
+        alternativesAndProfiles.addAll(inputs.getAlternativesIds());
+        alternativesAndProfiles.addAll(inputs.getProfilesIds());
+
+        for (String alternative: inputs.getAlternativesIds()) {
+            for (String profile: inputs.getProfilesIds()) {
+                putPreferencesIntoMap(inputs, errors, matrix, alternative, profile);
+            }
+        }
+
+        for (String profile: inputs.getProfilesIds()) {
+            for (String alternativeProfile: alternativesAndProfiles) {
+                putPreferencesIntoMap(inputs, errors, matrix, profile, alternativeProfile);
+            }
+        }
+    }
+
+    private static void putPreferencesIntoMap(Inputs inputs, ProgramExecutionResult errors,
+                                                 AlternativesMatrix<Double> matrix, String first, String second) {
+        inputs.partialPreferences.putIfAbsent(first, new LinkedHashMap<>());
+        inputs.partialPreferences.get(first).putIfAbsent(second, new LinkedHashMap<>());
+        Alternative alt1 = new Alternative(first);
+        Alternative alt2 = new Alternative(second);
+        Coord<Alternative, Alternative> coord = new Coord<>(alt1, alt2);
+        QualifiedValues<Double> values = matrix.getOrDefault(coord, null);
+
+        if (values == null) {
+            if (!first.equals(second)) {
+                errors.addError("List of partial preferences does not contain value for coord (" + first + "," + second + ")");
+            }
+            return;
+        }
+        if (values.size() != inputs.criteriaIds.size()) {
+            errors.addError("List of partial preferences does not contain correct criteria list");
+            return;
+        }
+
+        for (QualifiedValue<Double> value : values) {
+            if (inputs.criteriaIds.contains(value.id())) {
+                if (inputs.partialPreferences.get(first).get(second).containsKey(value.id())) {
+                    errors.addError("List of partial preferences contains duplicates of criteria");
+                    return;
+                } else {
+                    BigDecimal bigDecimalValue = BigDecimal.valueOf(value.getValue());
+                    inputs.partialPreferences.get(first).get(second).put(value.id(), bigDecimalValue);
+                }
+            }
+        }
+    }
+
+
+
+
+/*    protected static void checkAndExtractAlternativesFlows(Inputs inputs, XMCDA xmcda, ProgramExecutionResult errors) {
         if (xmcda.alternativesValuesList.size() != 1) {
             errors.addError("You need to provide 1 alternatives values list for net flows.");
             return;
@@ -302,10 +486,10 @@ public class InputsHandler {
             return;
         }
 
-        checkMissingValuesInNetFlows(inputs, errors);
+        checkMissingValuesInCriteriaWeights(inputs, errors);
     }
 
-    protected static void checkMissingValuesInNetFlows(Inputs inputs, ProgramExecutionResult errors) {
+    protected static void checkMissingValuesInCriteriaWeights(Inputs inputs, ProgramExecutionResult errors) {
         for (int j = 0; j < inputs.getAlternativesIds().size(); j++) {
             String alternativeId = inputs.getAlternativesIds().get(j);
             if (!inputs.getFlows().containsKey(alternativeId)) {
@@ -314,7 +498,7 @@ public class InputsHandler {
             }
         }
 
-        BigDecimal lastFlow = BigDecimal.valueOf(Double.MIN_VALUE);
+        BigDecimal lastFlow = BigDecimal.valueOf(-Double.MAX_VALUE);
         for (int i = 0; i < inputs.getProfilesIds().size(); i++) {
             String profileId = inputs.getProfilesIds().get(i);
             if (!inputs.getFlows().containsKey(profileId)) {
@@ -328,5 +512,5 @@ public class InputsHandler {
             }
             lastFlow = currentFlow;
         }
-    }
+    }*/
 }
--- PrometheeTri/src/pl/poznan/put/promethee/xmcda/PrometheeTriXMCDAv3.java
+++ PrometheeTri/src/pl/poznan/put/promethee/xmcda/PrometheeTriXMCDAv3.java
@@ -37,8 +37,14 @@ public class PrometheeTriXMCDAv3 {
         Utils.loadXMCDAv3(xmcda, new File(indir, "categories.xml"), true, executionResult, "categoriesValues");
         Referenceable.DefaultCreationObserver.currentMarker="categoriesProfiles";
         Utils.loadXMCDAv3(xmcda, new File(indir, "categories_profiles.xml"), true, executionResult, "categoriesProfiles");
-        Referenceable.DefaultCreationObserver.currentMarker="flows";
-        Utils.loadXMCDAv3(xmcda, new File(indir, "flows.xml"), true, executionResult, "alternativesValues");
+
+        Referenceable.DefaultCreationObserver.currentMarker="criteria";
+        Utils.loadXMCDAv3(xmcda, new File(indir, "criteria.xml"), true, executionResult, "criteria");
+        Referenceable.DefaultCreationObserver.currentMarker="weights";
+        Utils.loadXMCDAv3(xmcda, new File(indir, "weights.xml"), true, executionResult, "criteriaValues");
+        Referenceable.DefaultCreationObserver.currentMarker="preferences";
+        Utils.loadXMCDAv3(xmcda, new File(indir, "preferences.xml"), true, executionResult, "alternativesMatrix");
+
         Referenceable.DefaultCreationObserver.currentMarker="methodParameters";
         Utils.loadXMCDAv3(xmcda, new File(indir, "method_parameters.xml"), true, executionResult, "programParameters");
 
