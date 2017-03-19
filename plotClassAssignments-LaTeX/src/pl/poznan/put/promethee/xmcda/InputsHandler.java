d7efff991f31325e0609c63bfa60e74d1b4bff1d
Maciej Uniejewski <maciej.uniejewski@gmail.com>
2017-03-19 21:05:17 +0100

    [plotClassAssignments-LaTeX]:
    - added new input exception checking

--- plotClassAssignments-LaTeX/src/pl/poznan/put/promethee/xmcda/InputsHandler.java
+++ plotClassAssignments-LaTeX/src/pl/poznan/put/promethee/xmcda/InputsHandler.java
@@ -1,6 +1,7 @@
 package pl.poznan.put.promethee.xmcda;
 
 import org.xmcda.*;
+import pl.poznan.put.promethee.exceptions.InputDataException;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -73,52 +74,73 @@ public class InputsHandler {
 
     protected static Inputs checkInputs(XMCDA xmcda, ProgramExecutionResult errors) {
         Inputs inputs = new Inputs();
-        checkAndExtractAlternatives(inputs, xmcda, errors);
-        checkAndExtractCategories(inputs, xmcda, errors);
-        checkCategoriesRanking(inputs, xmcda, errors);
-        sortCategories(inputs);
-        checkAndExtractAssignments(inputs, xmcda, errors);
 
+        try {
+            checkAndExtractAlternatives(inputs, xmcda, errors);
+            checkAndExtractCategories(inputs, xmcda, errors);
+            checkCategoriesRanking(inputs, xmcda, errors);
+            sortCategories(inputs);
+            checkAndExtractAssignments(inputs, xmcda, errors);
+        } catch (InputDataException exception) {
+            //Just catch the exceptions and skip other functions
+        }
 
         return inputs;
     }
 
-    protected static void checkAndExtractAlternatives(Inputs inputs, XMCDA xmcda, ProgramExecutionResult errors) {
+    protected static void checkAndExtractAlternatives(Inputs inputs, XMCDA xmcda, ProgramExecutionResult errors) throws InputDataException {
         if (xmcda.alternatives.isEmpty()) {
-            errors.addError("No alternatives list has been supplied");
-        } else {
-            List<String> alternativesIds = xmcda.alternatives.getActiveAlternatives().stream().filter(a -> "alternatives".equals(a.getMarker())).map(
-                    Alternative::id).collect(Collectors.toList());
-            if (alternativesIds.isEmpty())
-                errors.addError("The alternatives list can not be empty");
-
-            inputs.alternativesIds = alternativesIds;
+            String errorMessage = "No alternatives list has been supplied";
+            errors.addError(errorMessage);
+            throw new InputDataException(errorMessage);
         }
+        List<String> alternativesIds = xmcda.alternatives.getActiveAlternatives().stream().filter(a -> "alternatives".equals(a.getMarker())).map(
+                Alternative::id).collect(Collectors.toList());
+        if (alternativesIds.isEmpty()) {
+            String errorMessage = "The alternatives list can not be empty";
+            errors.addError(errorMessage);
+            throw new InputDataException(errorMessage);
+        }
+        inputs.alternativesIds = alternativesIds;
     }
 
-    protected static void checkAndExtractCategories(Inputs inputs, XMCDA xmcda, ProgramExecutionResult errors) {
+    protected static void checkAndExtractCategories(Inputs inputs, XMCDA xmcda, ProgramExecutionResult errors) throws InputDataException {
         if (xmcda.categories.isEmpty()) {
-            errors.addError("No categories has been supplied.");
-        } else if (xmcda.categories.size() == 1) {
-            errors.addError("You should supply at least 2 categories.");
-        } else {
-            List<String> categories = xmcda.categories.getActiveCategories().stream().filter(a -> "categories".equals(a.getMarker())).map(
-                    Category::id).collect(Collectors.toList());
-            inputs.setCategoriesIds(categories);
-            if (categories.isEmpty())
-                errors.addError("The category list can not be empty.");
+            String errorMessage = "No categories has been supplied.";
+            errors.addError(errorMessage);
+            throw new InputDataException(errorMessage);
+        }
+        if (xmcda.categories.size() == 1) {
+            String errorMessage = "You should supply at least 2 categories.";
+            errors.addError(errorMessage);
+            throw new InputDataException(errorMessage);
+        }
+        List<String> categories = xmcda.categories.getActiveCategories().stream().filter(a -> "categories".equals(a.getMarker())).map(
+                Category::id).collect(Collectors.toList());
+        inputs.setCategoriesIds(categories);
+        if (categories.isEmpty()) {
+            String errorMessage = "The category list can not be empty.";
+            errors.addError(errorMessage);
+            throw new InputDataException(errorMessage);
         }
     }
 
-    protected static void checkCategoriesRanking(Inputs inputs, XMCDA xmcda, ProgramExecutionResult errors) {
+    protected static void checkCategoriesRanking(Inputs inputs, XMCDA xmcda, ProgramExecutionResult errors) throws InputDataException {
         if (xmcda.categoriesValuesList.isEmpty()) {
-            errors.addError("No categories values list has been supplied");
-        } else if (xmcda.categoriesValuesList.size() > 1) {
-            errors.addError("More than one categories values list has been supplied");
+            String errorMessage = "No categories values list has been supplied";
+            errors.addError(errorMessage);
+            throw new InputDataException(errorMessage);
+        }
+        if (xmcda.categoriesValuesList.size() > 1) {
+            String errorMessage = "More than one categories values list has been supplied";
+            errors.addError(errorMessage);
+            throw new InputDataException(errorMessage);
         }
         CategoriesValues categoriesValuesList = xmcda.categoriesValuesList.get(0);
         if (!categoriesValuesList.isNumeric()) {
-            errors.addError("Each of the categories ranks must be integer");
+            String errorMessage = "Each of the categories ranks must be integer";
+            errors.addError(errorMessage);
+            throw new InputDataException(errorMessage);
         }
         Map<String, Integer> categoriesValues = new LinkedHashMap<>();
 
@@ -127,7 +149,7 @@ public class InputsHandler {
     }
 
     public static void checkRanks(CategoriesValues categoriesValuesList, Map<String, Integer> categoriesValues,
-                                  Inputs inputs, XMCDA xmcda, ProgramExecutionResult errors) {
+                                  Inputs inputs, XMCDA xmcda, ProgramExecutionResult errors) throws InputDataException {
         try {
             CategoriesValues<Integer> categoriesValuesClass = categoriesValuesList.convertTo(Integer.class);
             xmcda.categoriesValuesList.set(0, categoriesValuesClass);
@@ -145,26 +167,32 @@ public class InputsHandler {
                 categoriesValues.put(a.getKey().id(), a.getValue().get(0).getValue());
             }
             if (min != 1) {
-                errors.addError("Minimal rank should be equal to 1.");
-                return;
+                String errorMessage = "Minimal rank should be equal to 1.";
+                errors.addError(errorMessage);
+                throw new InputDataException(errorMessage);
             }
             if (max != inputs.categoriesIds.size()) {
-                errors.addError("Maximal rank should be equal to number of categories.");
-                return;
+                String errorMessage = "Maximal rank should be equal to number of categories.";
+                errors.addError(errorMessage);
+                throw new InputDataException(errorMessage);
             }
-
             inputs.setCategoriesRanking(categoriesValues);
+        } catch (InputDataException e) {
+            throw e;
         } catch (Exception e) {
-            errors.addError("An error occurred: " + e + ". Remember that each rank has to be integer.");
+            String errorMessage = "An error occurred while checking the categories rank. Remember that each rank has to be integer.";
+            errors.addError(errorMessage);
+            throw new InputDataException(errorMessage);
         }
     }
 
-    public static void findRankingDuplicates(Map<String, Integer> categoriesValues, ProgramExecutionResult errors) {
+    public static void findRankingDuplicates(Map<String, Integer> categoriesValues, ProgramExecutionResult errors) throws InputDataException {
         for (Map.Entry<String, Integer> categoryA : categoriesValues.entrySet()) {
             for (Map.Entry<String, Integer> categoryB : categoriesValues.entrySet()) {
-                if (categoryA.getValue() == categoryB.getValue() && categoryA.getKey() != categoryB.getKey()) {
-                    errors.addError("There can not be two categories with the same rank.");
-                    return;
+                if (categoryA.getValue().equals(categoryB.getValue()) && !categoryA.getKey().equals(categoryB.getKey())) {
+                    String errorMessage = "There can not be two categories with the same rank.";
+                    errors.addError(errorMessage);
+                    throw new InputDataException(errorMessage);
                 }
             }
         }
@@ -177,19 +205,17 @@ public class InputsHandler {
         Collections.sort(inputs.categoriesIds, (o1, o2) -> inputs.categoriesRanking.get(o1) - inputs.categoriesRanking.get(o2));
     }
 
-    protected static void checkAndExtractAssignments(Inputs inputs, XMCDA xmcda, ProgramExecutionResult errors) {
+    protected static void checkAndExtractAssignments(Inputs inputs, XMCDA xmcda, ProgramExecutionResult errors) throws InputDataException {
         if (xmcda.alternativesAssignmentsList.size() != 1) {
-            errors.addError("You need to provide one list of alternatives assignments.");
-            return;
+            String errorMessage = "You need to provide one list of alternatives assignments.";
+            errors.addError(errorMessage);
+            throw new InputDataException(errorMessage);
         }
 
         if (xmcda.alternativesAssignmentsList.get(0).isEmpty()) {
-            errors.addError("Assignments list can not be empty.");
-            return;
-        }
-
-        if (inputs.getCategoriesRanking() == null) {
-            return;
+            String errorMessage = "Assignments list can not be empty.";
+            errors.addError(errorMessage);
+            throw new InputDataException(errorMessage);
         }
 
         Set<String> categoriesSet = new HashSet<>();
@@ -202,7 +228,7 @@ public class InputsHandler {
 
             if (assignment == null) {
                 errors.addError(ALTERNATIVES_TAG_ERROR);
-                return;
+                throw new InputDataException(ALTERNATIVES_TAG_ERROR);
             }
             String alternativeId = assignment.getAlternative().id();
             if (assignment.getCategory() == null) {
@@ -217,18 +243,19 @@ public class InputsHandler {
     }
 
     protected static void checkAndExtractIndirectInAssignment(AlternativeAssignment assignment, Set<String> categoriesSet,
-                                                              Map<String, String> assignmentMap, Inputs inputs, ProgramExecutionResult errors) {
+                                                              Map<String, String> assignmentMap, Inputs inputs, ProgramExecutionResult errors) throws InputDataException {
         if (assignment.getCategoryInterval() == null || assignment.getCategoryInterval().getLowerBound() == null ||
                 assignment.getCategoryInterval().getUpperBound() == null) {
             errors.addError(ALTERNATIVES_TAG_ERROR);
-            return;
+            throw new InputDataException(ALTERNATIVES_TAG_ERROR);
         }
 
         String lowerCategory = assignment.getCategoryInterval().getLowerBound().id();
         String upperCategory = assignment.getCategoryInterval().getUpperBound().id();
         if (!categoriesSet.contains(lowerCategory) || !categoriesSet.contains(upperCategory)) {
-            errors.addError("There are some categories in assignment list that were not be added to categories list.");
-            return;
+            String errorMessage = "There are some categories in assignment list that were not be added to categories list.";
+            errors.addError(errorMessage);
+            throw new InputDataException(errorMessage);
         }
         checkCategories(lowerCategory, upperCategory, inputs, errors);
 
@@ -237,28 +264,30 @@ public class InputsHandler {
         assignmentMap.put("UPPER", upperCategory);
     }
 
-    protected static void checkCategories(String lower, String upper, Inputs inputs, ProgramExecutionResult errors) {
+    protected static void checkCategories(String lower, String upper, Inputs inputs, ProgramExecutionResult errors) throws InputDataException {
         Integer lowerRank = inputs.getCategoriesRanking().get(lower);
         Integer upperRank = inputs.getCategoriesRanking().get(upper);
 
         if (lowerRank == null || upperRank == null || lowerRank > upperRank) {
-            errors.addError("Each lower category in assignments should have better mark then upper category for the same alternative.");
-            return;
+            String errorMessage = "Each lower category in assignments should have better mark then upper category for the same alternative.";
+            errors.addError(errorMessage);
+            throw new InputDataException(errorMessage);
         }
     }
 
     protected static void checkAndExtractDirectInAssignment(AlternativeAssignment assignment, Set<String> categoriesSet,
-                                                            Map<String, String> assignmentMap, Inputs inputs, ProgramExecutionResult errors) {
+                                                            Map<String, String> assignmentMap, Inputs inputs, ProgramExecutionResult errors) throws InputDataException {
         if (assignment.getCategory().id() == null) {
             errors.addError(ALTERNATIVES_TAG_ERROR);
-            return;
+            throw new InputDataException(ALTERNATIVES_TAG_ERROR);
         }
 
         String category = assignment.getCategory().id();
 
         if (!categoriesSet.contains(category)) {
-            errors.addError("There are some categories in assignment list that were not be added to categories list.");
-            return;
+            String errorMessage = "There are some categories in assignment list that were not be added to categories list.";
+            errors.addError(errorMessage);
+            throw new InputDataException(errorMessage);
         }
         if (inputs.getAssignmentType() == null) {
             inputs.setAssignmentType("direct");
@@ -267,12 +296,13 @@ public class InputsHandler {
         assignmentMap.put("UPPER", category);
     }
 
-    protected static void checkAlternativesInAssignments(Inputs inputs, ProgramExecutionResult errors) {
+    protected static void checkAlternativesInAssignments(Inputs inputs, ProgramExecutionResult errors) throws InputDataException {
         for (int i = 0; i < inputs.getAlternativesIds().size(); i++) {
             String alternativeId = inputs.getAlternativesIds().get(i);
             if (inputs.assignments.get(alternativeId) == null) {
-                errors.addError("There are some missing alternatives in assignment list.");
-                return;
+                String errorMessage = "There are some missing alternatives in assignment list.";
+                errors.addError(errorMessage);
+                throw new InputDataException(errorMessage);
             }
         }
     }
